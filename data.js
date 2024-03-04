4239a1f7c43e5e522997f0c4c17f27606ae6293f
Kirti Salunkhe <kirtisalunkhe15@gmail.com>
2024-03-05 00:28:35 +0530

    code pushed

---
+++ data.js
@@ -0,0 +1,67 @@
+import africa from './assets/africa.png'
+import auarium from './assets/aquarium.png'
+import coimbatore from './assets/coimbatore.png'
+import kerala from './assets/kerala.png'
+import kunchikal from './assets/kunchikal.png'
+import lakhadweep from './assets/lakshadweep.png'
+import manali from './assets/manali.png'
+import tamilnadu from './assets/tamilnadu.png'
+export const tourist_package=[
+    {
+        "id":1,
+        "image":africa,
+        "title":"Africa",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":2,
+        "image":auarium,
+        "title":"Aquarium",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":3,
+        "image":coimbatore,
+        "title":"Coimbatore",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":4,
+        "image":kerala,
+        "title":"Kerala",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":5,
+        "image":kunchikal,
+        "title":"Kunchikal",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":6,
+        "image":lakhadweep,
+        "title":"Lakhadweep",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":7,
+        "image":manali,
+        "title":"Manali",
+        "description":"",
+        "cost":"1000rs"
+    },
+    {
+        "id":8,
+        "image":tamilnadu,
+        "title":"Tamilnadu",
+        "description":"",
+        "cost":"1000rs"
+    }
+   
+]
